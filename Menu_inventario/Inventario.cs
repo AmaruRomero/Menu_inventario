0c678098af9d5727cd32ccde8befaf75d1e4e9b1
LABORATORIO 2 <LABORATORIO 2@DESKTOP-ASRVHQR>
2024-11-08 09:38:14 -0600

    Agregar archivos de proyecto.

---
+++ Menu_inventario/Inventario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu_inventario
+{
+    public class Inventario
+    {
+        private List<Producto> productos;
+
+        public Inventario()
+        {
+            productos = new List<Producto>();
+        }
+
+        public void AgregarProducto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+        public IEnumerable<Producto> Filtrar_Ordenar_Productos(decimal preciominimo)
+        {
+            return productos
+            .Where(p => p.Precio > preciominimo)
+            .OrderBy(p => p.Precio);
+        }
+      
+    }
+}
---
+++ Menu_inventario/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Menu_inventario;
+
+namespace Menu_inventario
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Inventario Inventario_de_Productos = new Inventario();
+            Console.WriteLine("Bienvenidos al sistema de inventario de productos");
+            Console.WriteLine("Cuantos productos desea ingresar?: ");
+            int cant = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cant; i++)
+            {
+                Console.WriteLine($"\nProducto {i + 1}");
+                Console.WriteLine("Nombre: ");
+                string? nombre = Console.ReadLine();
+
+                Console.WriteLine("Precio: ");
+                decimal precio = decimal.Parse(Console.ReadLine());
+
+                Producto product = new Producto(nombre, precio);
+                Inventario_de_Productos.AgregarProducto(product);
+            }
+
+            Console.Write("Ingrese el precio minimo para filtrar los productos: ");
+            decimal preciominimo = decimal.Parse(Console.ReadLine());
+
+            var ProductosFiltrados = Inventario_de_Productos.Filtrar_Ordenar_Productos(preciominimo);
+
+            Console.WriteLine("Productos filtrados y ordenados por precio");
+            foreach (var producto in ProductosFiltrados)
+            {
+                Console.WriteLine(producto);
+            }
+        }
+    }
+}
